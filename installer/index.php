c4c39a21aea5f1ccad73188031556f073a313d04
Joby Elliott <code@byjoby.com>
2022-01-30 21:42:07 -0700

    database setup wizard

--- installer/index.php
+++ installer/index.php
@@ -1,14 +1,23 @@
 <?php
 
 use DigraphCMS\Config;
+use DigraphCMS\Context;
 use DigraphCMS\Digraph;
+use DigraphCMS\URL\URL;
+use Flatrr\SelfReferencingFlatArray;
 
 @session_start();
 if (is_file(__DIR__ . '/../vendor/autoload.php')) {
+    define('DIGRAPH_LOADED', true);
     require_once __DIR__ . '/../vendor/autoload.php';
     Digraph::initialize(function () {
         Config::set('paths.root', realpath(__DIR__ . '/..'));
+        Config::set('paths.web', realpath(__DIR__ . '/../web'));
     });
+    Context::url(Digraph::actualUrl());
+    Context::request(Digraph::actualRequest());
+} else {
+    define('DIGRAPH_LOADED', false);
 }
 ?>
 <!DOCTYPE html>
@@ -22,7 +31,10 @@ if (is_file(__DIR__ . '/../vendor/autoload.php')) {
 </head>
 
 <body>
-    <?php Wizard::run(); ?>
+    <?php
+    Wizard::initialize();
+    Wizard::run();
+    ?>
 </body>
 
 </html>
@@ -43,6 +55,21 @@ class Wizard
     const ERROR_SKIPPABLE = 1;
     protected static $errorMessages = [];
     protected static $confirmationMessages = [];
+    protected static $config_global, $config_env, $config_combined;
+
+    public static function initialize()
+    {
+        static::$config_global = new SelfReferencingFlatArray(
+            json_decode(file_get_contents(__DIR__ . '/../digraph.json'), true)
+        );
+        static::$config_env = new SelfReferencingFlatArray();
+        if (file_exists(__DIR__ . '/../digraph-env.json') && $contents = file_get_contents(__DIR__ . '/../digraph-env.json')) {
+            static::$config_env->merge(json_decode($contents, true));
+        }
+        static::$config_combined = clone static::$config_global;
+        static::$config_combined->merge(static::$config_env->get(null, true));
+    }
+
     public static function run()
     {
         foreach (static::INSTALL_STEPS as $stepNumber => $stepName) {
@@ -93,6 +120,62 @@ class Wizard
             }
         }
     }
+
+    public static function getConfig(string $key = null)
+    {
+        return static::$config_combined->get($key);
+    }
+
+    public static function setConfig(string $key, $value, $global = false)
+    {
+        if ($global) {
+            static::$config_global->set($key, $value);
+            file_put_contents(
+                __DIR__ . '/../digraph.json',
+                json_encode(
+                    static::$config_global->get(null, true),
+                    JSON_PRETTY_PRINT
+                )
+            );
+        } else {
+            static::$config_env->set($key, $value);
+            file_put_contents(
+                __DIR__ . '/../digraph-env.json',
+                json_encode(
+                    static::$config_env->get(null, true),
+                    JSON_PRETTY_PRINT
+                )
+            );
+        }
+        static::$config_combined = clone static::$config_global;
+        static::$config_combined->merge(static::$config_env->get(null, true));
+    }
+
+    public static function unsetConfig(string $key, $global = false)
+    {
+        if ($global) {
+            static::$config_global->unset($key);
+            file_put_contents(
+                __DIR__ . '/../digraph.json',
+                json_encode(
+                    static::$config_global->get(null, true),
+                    JSON_PRETTY_PRINT
+                )
+            );
+        } else {
+            static::$config_env->unset($key);
+            file_put_contents(
+                __DIR__ . '/../digraph-env.json',
+                json_encode(
+                    static::$config_env->get(null, true),
+                    JSON_PRETTY_PRINT
+                )
+            );
+        }
+        static::$config_combined = clone static::$config_global;
+        static::$config_combined->merge(static::$config_env->get(null, true));
+    }
+
     public static function skippableError(string $message = null)
     {
         if ($message) {
@@ -100,6 +183,7 @@ class Wizard
         }
         static::stepError(static::ERROR_SKIPPABLE);
     }
+
     public static function error(string $message = null)
     {
         if ($message) {
@@ -107,10 +191,12 @@ class Wizard
         }
         static::stepError(static::ERROR_INCOMPLETE);
     }
+
     public static function confirmation(string $message)
     {
         static::$confirmationMessages[] = $message;
     }
+
     public static function stepError(int $set = null): int
     {
         static $stepError = 0;
@@ -119,6 +205,7 @@ class Wizard
         }
         return $stepError;
     }
+
     public static function currentStep(int $set = null): int
     {
         $_SESSION['currentStep'] = @$_GET['currentStep'] ?? @$_SESSION['currentStep'] ?? 0;
--- installer/steps/database_setup.php
+++ installer/steps/database_setup.php
@@ -1,3 +1,79 @@
 <?php
 
-Wizard::error('Not implemented');
+use DigraphCMS\Config;
+use DigraphCMS\DB\DB;
+use DigraphCMS\HTML\Forms\Field;
+use DigraphCMS\HTML\Forms\FormWrapper;
+
+$_SESSION['dbmode'] = @$_GET['dbmode'] ?? @$_SESSION['dbmode'];
+$displayBack = true;
+
+switch ($_SESSION['dbmode']) {
+    default:
+        $displayBack = false;
+        Wizard::stepError(Wizard::ERROR_INCOMPLETE);
+        echo "<h2>Choose database location</h2>";
+        printf(
+            "<p><a href='?dbmode=sqlite' class='button'>Use a SQLite database file</a><br><small>Database will be stored in a SQLite file in <code>%s</code></small></p>",
+            Config::get('paths.storage')
+        );
+        printf(
+            "<p><a href='?dbmode=connect' class='button'>Connect to a database server</a><br><small>Define the connection parameters to use a separate database server</small></p>"
+        );
+        break;
+    case 'sqlite':
+        Wizard::unsetConfig('db');
+        Wizard::confirmation('Database setting written to <code>digraph-env.json</code>');
+        echo '<p>No configuration is necessary to use a SQLite database.</p>';
+        break;
+    case 'connect':
+        $host = (new Field('Host'))
+            ->setRequired(true)
+            ->setDefault(Wizard::getConfig('db.host'));
+        $port = (new Field('Port (if different from default)'))
+            ->setDefault(Wizard::getConfig('db.port'));
+        $name = (new Field('Database name'))
+            ->setRequired(true)
+            ->setDefault(Wizard::getConfig('db.name'));
+        $user = (new Field('Username'))
+            ->setDefault(Wizard::getConfig('db.user'));
+        $pass = (new Field('Password'))
+            ->setDefault(Wizard::getConfig('db.pass'));
+        $form = (new FormWrapper())
+            ->addChild($host)
+            ->addChild($port)
+            ->addChild($name)
+            ->addChild($user)
+            ->addChild($pass);
+        $form->token()->setCSRF(false);
+        $form->button()->setText('Check database configuration');
+        if ($form->ready()) {
+            $dbConfig = [
+                'adapter' => 'mysql',
+                'host' => $host->value(),
+                'port' => $port->value() ? $port->value() : null,
+                'name' => $name->value() ? $name->value() : null,
+                'user' => $user->value() ? $user->value() : null,
+                'pass' => $pass->value() ? $pass->value() : null
+            ];
+            try {
+                Config::set('db', 'false');
+                Config::set('db', $dbConfig);
+                DB::pdo();
+                Wizard::confirmation('Database connection successful');
+                Wizard::setConfig('db', $dbConfig);
+                Wizard::confirmation('Database setting written to <code>digraph-env.json</code>');
+            } catch (\Throwable $th) {
+                Wizard::error('Error: ' . $th->getMessage());
+            }
+        } else {
+            Wizard::stepError(Wizard::ERROR_INCOMPLETE);
+        }
+        echo $form;
+        break;
+}
+
+if ($displayBack) {
+    echo "<hr>";
+    echo "<a href='?dbmode=' class='button'>Back to database location selection</a>";
+}
